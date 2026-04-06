7e4a6b8a7811cfcc42b32cad8b06eb40b5137635
ApostolOleg <apostololeg0@gmail.com>
2026-04-06 18:55:23 +0300

    feat: implement enqueue, dequeue, and peek methods in PriorityDeque class (#56)

--- src/shared/libs/priorityDeque.js
+++ src/shared/libs/priorityDeque.js
@@ -2,18 +2,45 @@ export class PriorityDeque {
 	constructor() {
 		this.items = [];
 	}
-
-	enqueue(item, priority = 0) {}
-
-	dequeue(type = "oldest") {}
-
-	peek(type = "oldest") {}
-
+    
 	get size() {
 		return this.items.length;
 	}
 
 	isEmpty() {
 		return this.size === 0;
+	}
+
+	enqueue(item, priority = 0) {
+		items.push({ item, priority });
+		items.sort((a, b) => b.priority - a.priority);
+	}
+
+	dequeue(type = "highest") {
+		if (this.isEmpty()) return null;
+
+		if (type === "highest") {
+			return this.items.shift().item;
+		}
+
+		if (type === "lowest") {
+			return this.items.pop().item;
+		}
+
+		return null;
+	}
+
+	peek(type = "lowest") {
+		if (this.isEmpty()) return null;
+
+		if (type === "highest") {
+			return this.items[0].item;
+		}
+
+		if (type === "lowest") {
+			return this.items[this.size - 1].item;
+		}
+
+		return null;
 	}
 }
