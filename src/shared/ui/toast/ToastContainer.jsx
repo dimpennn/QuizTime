6f5672cd7ebfccca92691d928f517334459c7db7
ApostolOleg <apostololeg0@gmail.com>
2026-04-12 16:29:33 +0300

    style: update ToastContainer to display toasts in reverse order for improved visibility (#56)

--- src/shared/ui/toast/ToastContainer.jsx
+++ src/shared/ui/toast/ToastContainer.jsx
@@ -7,7 +7,7 @@ export default function ToastContainer() {
 	if (toasts.length === 0) return null;
 
 	return (
-		<div className="fixed inset-x-6 bottom-4 z-50 flex flex-col pointer-events-none sm:inset-x-auto sm:right-4 sm:w-80">
+		<div className="fixed inset-x-6 bottom-4 z-50 flex flex-col-reverse pointer-events-none sm:inset-x-auto sm:right-4 sm:w-80">
 			{toasts.map((toast) => (
 				<Toast
 					key={toast.id}
